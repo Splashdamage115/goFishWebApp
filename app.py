9f10009dc3a95508266ba6264d9945c80d2fa2ef
davidStrikaitis <davidstrik282@gmail.com>
2024-10-22 11:30:42 +0100

    added card animations, removed text for card request, added images, moved no to deck, added lying

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, flash
+from flask import Flask, render_template, session, flash, redirect, request
 import cards
 import random
 
@@ -6,11 +6,32 @@ app = Flask(__name__)
 
 app.secret_key = " b0981x2'7rbpxr09483r431x 0nN740P9T4XNC8243-ny8p29fn32480-2n8p249-3fg24ny0fn209]'234]#of432n2f[08]"
 
+def checkPlayerHasCard(selected_value):
+    """
+    check if the player has the card requested
+    """
+    value = session["computerRequest"][: session["computerRequest"].find(" ")]
+    if selected_value == value.lower():
+        return True
+    return False
+
+def checkPlayerGoFish():
+    """
+    check if the player has the card requested
+    """
+    value = session["computerRequest"][: session["computerRequest"].find(" ")]
+    # swap code
+    for n, card in enumerate(session["player"]):
+        if card.startswith(value):
+            return True
+    return False
+
 @app.get("/startgame")
 @app.get("/")
 def start():
     resetGame()
     card_images = [card.lower().replace(" ", "_") + ".png" for card in session["player"]]
+
     return render_template(
         "startgame.html",
         title = "Welcome to GoFish for the web!",
@@ -18,12 +39,19 @@ def start():
         n_computer = len(session["computer"]), # available in the template as {{  n_computer  }}
         deck = len(session["deck"]),
         playerPair = len(session["player_pairs"]),
-        computerPair = len(session["computer_pairs"])
+        computerPair = len(session["computer_pairs"]),
+        resolveCard = "none.png",
+        animateCard = "none.png",
+        disolveCard = "welcomCard.png"
     )
+
+
 
 @app.get("/select/<value>")
 def processCardSelection(value):
     found_it = False
+    drawn = "none.png"
+    drawType = "none.png"
     for n, card in enumerate(session["computer"]):
         if card.startswith(value):
             found_it = n
@@ -34,29 +62,29 @@ def processCardSelection(value):
         flash("\nGo Fish\n")
         session["player"].append(session["deck"].pop())
         flash(f"You drew a {session["player"][-1]}")
-        ## what to do when there are no more cards in the deck
-        ## if len(session["deck"]) == 0:
-        ##     break
+        drawType = "goFishCard.png"
     else:
         # swap code
         flash(f"Here is your card: {session["computer"][n]}.")
         session["player"].append(session["computer"].pop(n))
-    
+
+    drawn = session["player"][-1]
+
     session["player"], pairs = cards.identify_remove_pairs(session["player"])
     session["player_pairs"].extend(pairs)
 
-    ## what to do when the player or computer has won!
-    ## if len(player) == 0:
-    ##     print("The game is over. The player Won!")
-    ##     break
-    ## if len(computer) == 0:
-    ##     print("The game is over. The computer Won!")
-    ##     break
+    if len(session["player"]) == 0 or len(session["computer"]) == 0 or len(session["deck"]) == 0:
+         return redirect("/gameOver")
 
+    drawn = drawn.lower().replace(" ", "_") + ".png"
     card_images = [card.lower().replace(" ", "_") + ".png" for card in session["player"]]
 
+    
     card = random.choice(session["computer"])
+    session["computerRequest"] = card
     the_value = card[: card.find(" ")]
+    chosen = the_value.lower() + "_request.png"
+    
 
     return render_template(
         "pickCard.html",
@@ -66,23 +94,64 @@ def processCardSelection(value):
         n_computer = len(session["computer"]), # available in the template as {{  n_computer  }}
         deck = len(session["deck"]),
         playerPair = len(session["player_pairs"]),
-        computerPair = len(session["computer_pairs"])
+        computerPair = len(session["computer_pairs"]),
+        resolveCard = chosen,
+        animateCard = drawn,
+        disolveCard = drawType
+    )
+
+@app.get("/liarPage")
+def liarPage():
+    drawType = "none.png"
+    drawn = session["player"][-1]
+    drawn = drawn.lower().replace(" ", "_") + ".png"
+    card_images = [card.lower().replace(" ", "_") + ".png" for card in session["player"]]
+
+    
+    card = session["computerRequest"]
+    the_value = card[: card.find(" ")]
+    chosen = the_value.lower() + "_request.png"
+    
+
+    return render_template(
+        "pickCard.html",
+        title = "The Computer wants to Know",
+        value = the_value,
+        cards = card_images, # available in the template as {{  cards  }}
+        n_computer = len(session["computer"]), # available in the template as {{  n_computer  }}
+        deck = len(session["deck"]),
+        playerPair = len(session["player_pairs"]),
+        computerPair = len(session["computer_pairs"]),
+        resolveCard = chosen,
+        animateCard = drawn,
+        disolveCard = drawType
     )
 
 @app.get("/pick/<value>")
 def processCardHandOver(value):
+    face = "back.png"
     if value == "0":
         session["computer"].append(session["deck"].pop())
+        if checkPlayerGoFish():
+            return redirect("/liarPage")
     else:
+        if not checkPlayerHasCard(value):
+            return redirect("/liarPage")
         for n, card in enumerate(session["player"]):
                 if card.startswith(value.title()):
                     break
         session["computer"].append(session["player"].pop(n))
+        face = session["computer"][-1].lower().replace(" ", "_") + ".png"
 
     session["computer"], pairs = cards.identify_remove_pairs(session["computer"])
     session["computer_pairs"].extend(pairs)
 
+    if len(session["player"]) == 0 or len(session["computer"]) == 0 or len(session["deck"]) == 0:
+        return redirect("/gameOver")
+
     card_images = [card.lower().replace(" ", "_") + ".png" for card in session["player"]]
+
+
     
     return render_template(
         "startgame.html",
@@ -91,7 +160,10 @@ def processCardHandOver(value):
         n_computer = len(session["computer"]), # available in the template as {{  n_computer  }}
         deck = len(session["deck"]),
         playerPair = len(session["player_pairs"]),
-        computerPair = len(session["computer_pairs"])
+        computerPair = len(session["computer_pairs"]),
+        resolveCard = "none.png",
+        animateCard = face,
+        disolveCard = "none.png"
     )
 
 
